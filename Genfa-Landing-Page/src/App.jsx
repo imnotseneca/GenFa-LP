5c76f8b51ffb66fa164e20e6a5183192080780a0
BrunoC <corraobruno13@gmail.com>
2023-10-24 22:23:06 -0300

    small change to opacity for  white__gradient class from index.css

--- Genfa-Landing-Page/src/App.jsx
+++ Genfa-Landing-Page/src/App.jsx
@@ -32,11 +32,8 @@ const App = () => {
       <div className={` bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Stats />
-          {/* Business section starts */}
           <Business />
-          {/* Business section ends */}
           <Carousel />
-          {/* <Billing /> */}
           <Testimonials />
           <Clients />
           <CallToAction />
